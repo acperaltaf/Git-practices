e2417e907492dd4c3b44fff95172c1cb45d95272
Andres Peralta <114369126+acperaltaf@users.noreply.github.com>
2024-05-07 21:26:46 -0500

    Update codigo.py
    ...en donde todo es posible

--- codigo.py
+++ codigo.py
@@ -1,5 +1,5 @@
 name = 'Andres'
 surname = 'Peralta Fragozo'
 
-greet = f'Hola {name} {surname}. Bienvenido a la masion de los sueños.'
+greet = f'Hola {name} {surname}. Bienvenido a la masion de los sueños en donde todo es posible.'
 print(greet)
